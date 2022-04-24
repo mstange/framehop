6b2abc73258f625f24c36ccb2c647c689742f8db
Markus Stange <mstange.moz@gmail.com>
2022-04-24 13:42:37 -0400

    Add some tests for add_signed.rs.

--- src/add_signed.rs
+++ src/add_signed.rs
@@ -60,3 +60,40 @@ impl AddSigned for u32 {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::{checked_add_signed, wrapping_add_signed};
+
+    #[test]
+    fn test_wrapping() {
+        assert_eq!(wrapping_add_signed(1, 2), 3u64);
+        assert_eq!(wrapping_add_signed(2, 1), 3u64);
+        assert_eq!(wrapping_add_signed(5, -4), 1u64);
+        assert_eq!(wrapping_add_signed(5, -5), 0u64);
+        assert_eq!(wrapping_add_signed(u64::MAX - 5, 3), u64::MAX - 2);
+        assert_eq!(wrapping_add_signed(u64::MAX - 5, 5), u64::MAX);
+        assert_eq!(wrapping_add_signed(u64::MAX - 5, -5), u64::MAX - 10);
+        assert_eq!(wrapping_add_signed(1, -2), u64::MAX);
+        assert_eq!(wrapping_add_signed(2, -4), u64::MAX - 1);
+        assert_eq!(wrapping_add_signed(u64::MAX, 1), 0);
+        assert_eq!(wrapping_add_signed(u64::MAX - 5, 6), 0);
+        assert_eq!(wrapping_add_signed(u64::MAX - 5, 9), 3);
+    }
+
+    #[test]
+    fn test_checked() {
+        assert_eq!(checked_add_signed(1, 2), Some(3u64));
+        assert_eq!(checked_add_signed(2, 1), Some(3u64));
+        assert_eq!(checked_add_signed(5, -4), Some(1u64));
+        assert_eq!(checked_add_signed(5, -5), Some(0u64));
+        assert_eq!(checked_add_signed(u64::MAX - 5, 3), Some(u64::MAX - 2));
+        assert_eq!(checked_add_signed(u64::MAX - 5, 5), Some(u64::MAX));
+        assert_eq!(checked_add_signed(u64::MAX - 5, -5), Some(u64::MAX - 10));
+        assert_eq!(checked_add_signed(1u64, -2), None);
+        assert_eq!(checked_add_signed(2u64, -4), None);
+        assert_eq!(checked_add_signed(u64::MAX, 1), None);
+        assert_eq!(checked_add_signed(u64::MAX - 5, 6), None);
+        assert_eq!(checked_add_signed(u64::MAX - 5, 9), None);
+    }
+}
