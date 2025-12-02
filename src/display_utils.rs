b5a0ebb50cd6a20e6ede6413a5e5034144413c77
Markus Stange <mstange.moz@gmail.com>
2025-12-02 11:49:11 -0500

    Remove unused struct BinNum.

--- src/display_utils.rs
+++ src/display_utils.rs
@@ -1,17 +1,9 @@
-use core::fmt::{Binary, Debug, LowerHex};
+use core::fmt::{Debug, LowerHex};
 
 pub struct HexNum<N: LowerHex>(pub N);
 
 impl<N: LowerHex> Debug for HexNum<N> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         LowerHex::fmt(&self.0, f)
-    }
-}
-
-pub struct BinNum<N: Binary>(pub N);
-
-impl<N: Binary> Debug for BinNum<N> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        Binary::fmt(&self.0, f)
     }
 }
